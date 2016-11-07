70ba00322fb63e3ee3ce54f0c88421549f3bfaa1
Hildequias Junior <juniorwelling@yahoo.com.br>
2016-11-07 14:03:54 -0200

    updating our rocket sprite

--- Rocket/Classes/GameLayer.cpp
+++ Rocket/Classes/GameLayer.cpp
@@ -71,15 +71,141 @@ bool GameLayer::init()
 
 void GameLayer::update (float dt) {
     
+    if (!_running || _state != kGamePlay) return;
+    
+    
+    if (_lineContainer->getLineType() != LINE_NONE) {
+        _lineContainer->setTip (_rocket->getPosition() );
+    }
+    
+    //track collision with sides
+    if (_rocket->collidedWithSides()) {
+        // _lineContainer->setLineType ( LINE_NONE );
+    }
+    
+    _rocket->update(dt);
+    
+    //update jet particle so it follow rocket
+    if (!_jet->isActive()) _jet->resetSystem();
+    _jet->setRotation(_rocket->getRotation());
+    _jet->setPosition(_rocket->getPosition());
+    
+    
+    //update timers
+    _cometTimer += dt;
+    float newY;
+    if (_cometTimer > _cometInterval) {
+        _cometTimer = 0;
+        if (_comet->isVisible() == false) {
+            _comet->setPositionX(0);
+            newY = (float)rand()/((float)RAND_MAX/_screenSize.height * 0.6f) + _screenSize.height * 0.2f;
+            if (newY > _screenSize.height * 0.9f) newY = _screenSize.height * 0.9f;
+            _comet->setPositionY(newY);
+            _comet->setVisible(true);
+            _comet->resetSystem();
+        }
+    }
+    
+    if (_comet->isVisible()) {
+        
+        //collision with comet
+        if (pow(_comet->getPositionX() - _rocket->getPositionX(), 2) +
+            pow(_comet->getPositionY() - _rocket->getPositionY(), 2) <= pow (_rocket->getRadius() , 2)) {
+            if (_rocket->isVisible()) killPlayer();
+        }
+        _comet->setPositionX(_comet->getPositionX() + 50 * dt);
+        if (_comet->getPositionX() > _screenSize.width * 1.5f) {
+            _comet->stopSystem();
+            _comet->setVisible(false);
+        }
+    }
+    
+    _lineContainer->update(dt);
+    _rocket->setOpacity(_lineContainer->getEnergy() * 255);
+    
+    //collision with planets
+    for (auto planet : _planets)
+    {
+        if (pow(planet->getPositionX() - _rocket->getPositionX(), 2)
+            + pow(planet->getPositionY() - _rocket->getPositionY(), 2) <=
+            pow (_rocket->getRadius() * 0.8f + planet->getRadius() * 0.65f, 2)) {
+            
+            if (_rocket->isVisible()) killPlayer();
+            break;
+        }
+    }
+    
+    //collision with star
+    if (pow(_star->getPositionX() - _rocket->getPositionX(), 2)
+        + pow(_star->getPositionY() - _rocket->getPositionY(), 2) <=
+        pow (_rocket->getRadius() * 1.2f, 2)) {
+        
+        _pickup->setPosition(_star->getPosition());
+        _pickup->resetSystem();
+        if (_lineContainer->getEnergy() + 0.25f < 1) {
+            _lineContainer->setEnergy(_lineContainer->getEnergy() + 0.25f);
+        } else {
+            _lineContainer->setEnergy(1.0);
+        }
+        _rocket->setSpeed(_rocket->getSpeed() + 2);
+        _lineContainer->setEnergyDecrement(0.0002f);
+        SimpleAudioEngine::getInstance()->playEffect("pickup.wav");
+        resetStar();
+        
+        int points = 100 - _timeBetweenPickups;
+        if (points < 0) points = 0;
+        
+        _score += points;
+        _scoreDisplay->setString(String::createWithFormat("%i", _score)->getCString());
+        
+        _timeBetweenPickups = 0;
+    }
+    
+    _timeBetweenPickups += dt;
+    if (_lineContainer->getEnergy() == 0) {
+        if (_rocket->isVisible()) killPlayer();
+    }
 }
 
 bool GameLayer::onTouchBegan(Touch *touch, Event *event){
 	
+    if (!_running) return true;
+    
+    Point tap = touch->getLocation();
+    float dx = _rocket->getPositionX() - tap.x;
+    float dy = _rocket->getPositionY() - tap.y;
+    
+    if (dx * dx + dy * dy <= pow(_rocket->getRadius(), 2)) {
+        _lineContainer->setLineType(LINE_NONE);
+        _rocket->setRotationOrientation(ROTATE_NONE);
+        _drawing = true;
+    }
     return true;
 }
 
 void GameLayer::onTouchMoved(Touch *touch, Event *event){
     
+    if (!_running) return;
+    
+    if (_drawing) {
+        
+        Point tap = touch->getLocation();
+        
+        float dx = _rocket->getPositionX() - tap.x;
+        float dy = _rocket->getPositionY() - tap.y;
+        
+        if (dx * dx + dy * dy > pow (_minLineLength, 2)) {
+            
+            _rocket->select(true);
+            _lineContainer->setPivot ( tap );
+            _lineContainer->setLineType ( LINE_TEMP );
+            
+        } else {
+            
+            _rocket->select(false);
+            _lineContainer->setLineType ( LINE_NONE );
+        }
+    }
 }
 
 void GameLayer::onTouchEnded(Touch *touch, Event *event){
@@ -108,10 +234,65 @@ void GameLayer::onTouchEnded(Touch *touch, Event *event){
         //resetGame();
         return;
         
+    }else if (_state == kGamePaused) {
+        
+        _pauseBtn->setDisplayFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName ("btn_pause_off.png"));
+        _paused->setVisible(false);
+        _state = kGamePlay;
+        _running = true;
+        
+        return;
     }
     
     if (!_running) return;
-
+    
+    if(touch != nullptr) {
+        
+        Point tap = touch->getLocation();
+        
+        if (_pauseBtn->getBoundingBox().containsPoint(tap)) {
+            
+            _paused->setVisible(true);
+            _state = kGamePaused;
+            _pauseBtn->setDisplayFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName ("btn_pause_on.png"));
+            _running = false;
+            
+            return;
+        }
+        
+        //track if tapping on ship
+        _drawing = false;
+        _rocket->select(false);
+        //if we are showing a temp line
+        if (_lineContainer->getLineType() == LINE_TEMP) {
+            
+            //set up dashed line
+            _lineContainer->setPivot (tap);
+            _lineContainer->setLineLength ( _rocket->getPosition().distance( tap ) );
+            
+            //set up rocket
+            _rocket->setPivot (tap);
+            float circle_length = _lineContainer->getLineLength() * 2 * M_PI;
+            int iterations = floor(circle_length / _rocket->getSpeed());
+            _rocket->setAngularSpeed ( 2 * M_PI / iterations);
+            
+            Vec2 diff = _rocket->getPosition();
+            diff.subtract(_rocket->getPivot());
+            Point clockwise = diff.getRPerp();
+            
+            float dot =clockwise.dot(_rocket->getVector());
+            
+            if (dot > 0) {
+                _rocket->setAngularSpeed ( _rocket->getAngularSpeed() * -1 );
+                _rocket->setRotationOrientation ( ROTATE_CLOCKWISE );
+                _rocket->setTargetRotation ( CC_RADIANS_TO_DEGREES( atan2(clockwise.y, clockwise.x) ) );
+            } else {
+                _rocket->setRotationOrientation ( ROTATE_COUNTER );
+                _rocket->setTargetRotation ( CC_RADIANS_TO_DEGREES (atan2(-1 * clockwise.y, -1 * clockwise.x) ) );
+            }
+            _lineContainer->setLineType ( LINE_DASHED );
+        }
+    }
 }
 
 void GameLayer::resetGame () {
@@ -255,7 +436,26 @@ void GameLayer::createGameScreen () {
 
 
 void GameLayer::createParticles() {
-
+    
+    _jet = ParticleSystemQuad::create("jet.plist");
+    _jet->setSourcePosition(Vec2(-_rocket->getRadius() * 0.8f,0));
+    _jet->setAngle(180);
+    _jet->stopSystem();
+    this->addChild(_jet, kBackground);
+    
+    _boom = ParticleSystemQuad::create("boom.plist");
+    _boom->stopSystem();
+    this->addChild(_boom, kForeground);
+    
+    _comet = ParticleSystemQuad::create("comet.plist");
+    _comet->stopSystem();
+    _comet->setPosition(Vec2(0, _screenSize.height * 0.6f));
+    _comet->setVisible(false);
+    this->addChild(_comet, kForeground);
+    
+    _pickup = ParticleSystemQuad::create("plink.plist");
+    _pickup->stopSystem();
+    this->addChild(_pickup, kMiddleground);
 }
 
 void GameLayer::createStarGrid() {
